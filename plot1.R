2adab6dc3b23e967d6a484089527ea4d76b1fffc
jpuccia <joseph.puccia@pds.nl>
2015-07-26 13:11:59 -0500

    Creating repository and adding the first two plots and the scripts that created them.

---
+++ plot1.R
@@ -0,0 +1,24 @@
+## plot1.R
+##
+## Task: Create a plot that shows the total emissions of PM2.5 by year for the US
+##... to determine if emissions are increasing or decreasing.
+##
+
+plot1 <- function(){
+        ## Read the emissions data from the data folder under the working directory
+        nei <- readRDS("./data/summarySCC_PM25.rds")
+        
+        ## Melt the data down to group emissions by Pollutant and year.
+        neiMelt <- melt(nei, id=c("Pollutant", "year"), measure.vars=c("Emissions"))
+        
+        ## Sum the Emissions by Pollutant and Year
+        neiYearData <- dcast(neiMelt, year + Pollutant ~ variable,sum)
+        
+        ## Plot the data to a png file
+        png(filename = "plot1.png", width = 480, height = 480, units = "px")
+        plot(neiYearData$year, neiYearData$Emissions, type = "l", col = "red",
+             xlab="", ylab="Yearly Emissions (tons)", main = "Total Yearly Emissions for the US")
+
+        ## Close the device to plot to a png file with the same name as this function.
+        invisible(dev.off())
+}
---
+++ plot2.R
@@ -0,0 +1,25 @@
+## plot2.R
+##
+## Task: Create a plot that shows the total emissions of PM2.5 by year for the 
+##... city of Baltimore (fips=24510) to determine if emissions are increasing 
+##... or decreasing.
+##
+
+plot2 <- function(){
+        ## Read the emissions data from the data folder under the working directory
+        nei <- readRDS("./data/summarySCC_PM25.rds")
+        
+        ## Melt the data down to group Baltimore emissions by Pollutant and year.
+        neiMelt <- melt(nei[nei$fips==24510,], id=c("Pollutant", "year"), measure.vars=c("Emissions"))
+        
+        ## Sum the Emissions by Pollutant and Year
+        neiYearData <- dcast(neiMelt, year + Pollutant ~ variable,sum)
+        
+        ## Plot the data to a png file
+        png(filename = "plot2.png", width = 480, height = 480, units = "px")
+        plot(neiYearData$year, neiYearData$Emissions, type = "l", col = "blue",
+             xlab="", ylab="Yearly Emissions (tons)", main = "Total Yearly Emissions for Baltimore, MD")
+        
+        ## Close the device to plot to a png file with the same name as this function.
+        invisible(dev.off())
+}
